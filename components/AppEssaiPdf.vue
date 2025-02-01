e570641c57177b6e1ae6548fcff11959b82e499c
Kourylles <sitemarchand@outlook.fr>
2025-02-01 23:34:07 +0100

    Ajout de deux propositions pdfflip-costumes Alice

--- components/AppEssaiPdf.vue
+++ components/AppEssaiPdf.vue
@@ -6,13 +6,28 @@ const spectacleStore = useItemsUtabsStore().data;
   <div class="mx-auto mt-2 p-5 text-center">
     <p class="mt-5 text-white"></p>
     <NuxtLink to="/pdf/BookDocu1.pdf" target="_blank">
-      <UButton label="Doc" size="lg"></UButton>
+      <UButton label="Mémoires du Monde" size="lg"></UButton>
     </NuxtLink>
   </div>
-  <div class="mx-auto mt-2 p-5 text-center">
-    <p class="mt-5 text-white"></p>
-    <NuxtLink to="/pdf/flipPdf/Alice Oudet Costumes.html" target="_blank">
-      <UButton label="FlipPdf" size="lg"></UButton>
-    </NuxtLink>
+  <div class="flex justify-center">
+    <div class="mt-2 p-5 text-center">
+      <p class="mt-5 text-white"></p>
+      <NuxtLink to="/pdf/flipPdf1/Alice Oudet Costumes1.html" target="_blank">
+        <UButton label="Costumes-Proposition 1" size="lg"></UButton>
+      </NuxtLink>
+    </div>
+    <div class="mt-2 p-5 text-center">
+      <p class="mt-5 text-white"></p>
+      <NuxtLink to="/pdf/flipPdf2/Alice Oudet Costumes2.html" target="_blank">
+        <UButton label="Costumes-Proposition 2" size="lg"></UButton>
+      </NuxtLink>
+    </div>
+    <div class="mt-2 p-5 text-center">
+      <p class="mt-5 text-white"></p>
+      <NuxtLink to="/pdf/flipPdf3/Alice Oudet Costumes3.html" target="_blank">
+        <UButton label="Costumes-Proposition 3" size="lg"></UButton>
+      </NuxtLink>
+    </div>
+
   </div>
 </template>
